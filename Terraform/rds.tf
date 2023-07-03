19dd520d8837473feeb7c3fed177f95e4310c323
aamiz.juspay <aamiz.sameen@juspay.in>
2023-07-03 14:25:59 +0530

    New code updated

---
+++ Terraform/rds.tf
@@ -0,0 +1,29 @@
+resource "aws_db_subnet_group" "public-db-subnet" {
+  name       = "public_subnets"
+  subnet_ids = [aws_subnet.New-public-subnet-1[0].id, aws_subnet.New-public-subnet-1[1].id]
+
+  tags = {
+    Name = "PublicDbSubnet"
+  }
+}
+
+
+resource "aws_db_instance" "myinstance" {
+
+  engine               = "mysql"
+  identifier           = "my-rds"
+  allocated_storage    =  20
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = "admin"
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
+  skip_final_snapshot  = true
+  publicly_accessible =  false
+  db_subnet_group_name = "${aws_db_subnet_group.public-db-subnet.name}"  // Based on the subnet's vpc rds will be launched in that particular vpc
+
+  tags = {
+    Name = "POC"
+  }
+}
--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -2,10 +2,15 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr_block" {
-  default     = "10.0.1.0/24"
-  description = "description"
-}
+# variable "subnet_cidr_block_1" {
+#   default     = "10.0.1.0/24"
+#   description = "description"
+# }
+
+# variable "subnet_cidr_block_2" {
+#   default     = "10.0.2.0/24"
+#   description = "description"
+# }
 
 variable "instance_type" {
   default     = "t2.micro"
@@ -15,9 +20,21 @@ variable "volume_size" {
   default     = 10
 }
 
-variable "vpc_availability_zones" {
-  type = list(string)
-  default = ["ap-south-1a", "ap-south-1b","ap-south-1c"]
+variable "db_password" {
+  type        = string
+  description = "Set the password for the database"
+  sensitive = true
 }
 
+variable "subnet_cidrs_public" {
+  description = "Subnet CIDRs for public subnets (length must match configured availability_zones)"
+  default = ["10.0.10.0/24", "10.0.20.0/24"]
+  type = list(string)
+}
 
+variable "availability_zones" {
+  description = "AZs in this region to use"
+  default = ["ap-south-1a", "ap-south-1b"]
+  type = list(string)
+}
+   
