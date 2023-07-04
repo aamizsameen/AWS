27e8ceebb0ecb5de15bd20916341a7c1dd933dd7
aamiz.juspay <aamiz.sameen@juspay.in>
2023-07-04 15:12:19 +0530

    Code updated

--- Terraform/CreateEC2.tf
+++ Terraform/CreateEC2.tf
@@ -3,8 +3,15 @@ resource "aws_instance" "New-EC2-Instance" {
     instance_type = "${var.instance_type}"
     ami = "ami-012b9156f755804f5"
     vpc_security_group_ids = [aws_security_group.New-SG.id]
-    subnet_id = aws_subnet.New-public-subnet-1.id
-    //user_data = file(userdata.tpl)
+    subnet_id = aws_subnet.New-public-subnet-1[0].id
+    user_data = <<-EOF
+                    #!/bin/bash
+                    sudo yum update -y
+                    sudo yum install httpd -y
+                    sudo systemctl start httpd
+                    sudo systemctl enable httpd
+                    sudo bash -c 'Welcome from Amazon' > /var/www/html/index.html
+                    EOF
 
     root_block_device {
         volume_size = "${var.volume_size}"
