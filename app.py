6c53ec02cb8a6f7f13ca13d7f7df171810b79c7d
aamizsameen <43962517+aamizsameen@users.noreply.github.com>
2025-07-04 16:43:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-name = "World"
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-print("Hello,", name + "!")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello from Python!</h1>")
+
+HTTPServer(('', 80), Handler).serve_forever()
